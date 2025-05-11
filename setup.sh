37afb4face0ef07ae080e4cca5d4f635cc7ce128
0xsaymon <artem.simonenko93@gmail.com>
2025-05-11 15:13:00 +0300

    fix: enhance output messages in setup script for better user feedback

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # WireGuard VPN Setup Script
+echo "🐲 WireGuard VPN Setup Script"
 
+echo "⬇️ Installing WireGuard..."
 # install WireGuard
 sudo apt update
 sudo apt install wireguard -y
@@ -10,15 +12,17 @@ sudo apt install wireguard -y
 mkdir -p ./dist
 cd ./dist
 
+echo "🗝️ Generating keys..."
 # generate new server keys
 wg genkey | tee server_private.key | wg pubkey >server_public.key
-
 # generate client keys
 wg genkey | tee client_private.key | wg pubkey >client_public.key
 
 # Determine the public IP of the VPS
 PUBLIC_IP=$(hostname -I | cut -d' ' -f1)
+echo "🖥️ Public IP: $PUBLIC_IP"
 
+echo "📝 Creating server configuration..."
 # Create the server configuration /etc/wireguard/wg0.conf
 sudo mkdir -p /etc/wireguard
 sudo tee /etc/wireguard/wg0.conf >/dev/null <<EOF
@@ -35,6 +39,7 @@ PublicKey = $(cat client_public.key)
 AllowedIPs = 10.0.0.2/32
 EOF
 
+echo "🔑 Create client configuration..."
 # Create the ready-to-use client configuration client.conf
 tee client.conf >/dev/null <<EOF
 [Interface]
@@ -49,16 +54,20 @@ AllowedIPs = 0.0.0.0/0, ::/0
 PersistentKeepalive = 25
 EOF
 
-echo "✅ Success! Configs saved to ~/setup_wireguard/dist/"
+echo "💼 Configs saved to ~/setup_wireguard/dist/client.conf"
 
+echo "🔄 Configuring firewall..."
 # Enable IP forwarding
 echo "net.ipv4.ip_forward=1" | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p
 
+echo "🔒 Starting WireGuard..."
 # Start WireGuard
 sudo wg-quick up wg0
 sudo systemctl enable wg-quick@wg0
 
+echo "📝 Client configuration:"
+echo ""
 # Display the client configuration
 CONFIG=$(cat ~/setup_wireguard/dist/client.conf)
 echo "client config:"
